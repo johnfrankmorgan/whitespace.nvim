cf66591fc74db1d8e90769456574f8254b7ce8a6
gavin.xu <gavin.xu@sigmastar.com.cn>
2024-07-31 11:50:25 +0800

    Refine ignore terminal.

--- lua/whitespace-nvim/init.lua
+++ lua/whitespace-nvim/init.lua
@@ -7,19 +7,13 @@ local config = {
 
 local whitespace = {}
 
+whitespace.clean = function()
+  vim.cmd('match')
+end
+
 whitespace.highlight = function()
   if not vim.fn.hlexists(config.highlight) then
     error(string.format('highlight %s does not exist', config.highlight))
-  end
-
-  vim.cmd('match')
-
-  if config.ignore_terminal and vim.bo.buftype == 'terminal' then
-    return
-  end
-
-  if vim.tbl_contains(config.ignored_filetypes, vim.bo.filetype) then
-    return
   end
 
   local command = string.format([[match %s /\s\+$/]], config.highlight)
@@ -36,15 +30,28 @@ whitespace.trim = function()
   end
 end
 
+local function file_type()
+  if vim.tbl_contains(config.ignored_filetypes, vim.bo.filetype) then
+    whitespace.clean()
+    return
+  end
+  whitespace.highlight()
+end
+
+local function term_open()
+  if config.ignore_terminal then
+    whitespace.clean()
+  else
+    whitespace.highlight()
+  end
+end
+
 whitespace.setup = function(options)
   config = vim.tbl_extend('force', config, options or {})
 
-  vim.cmd [[
-    augroup whitespace_nvim
-      autocmd!
-      autocmd FileType * lua require('whitespace-nvim').highlight()
-    augroup END
-  ]]
+  vim.api.nvim_create_augroup('whitespace_nvim', {clear = true})
+  vim.api.nvim_create_autocmd('FileType', {group = 'whitespace_nvim', pattern = '*', callback = file_type})
+  vim.api.nvim_create_autocmd('TermOpen', {group = 'whitespace_nvim', pattern = '*', callback = term_open})
 end
 
 return whitespace
