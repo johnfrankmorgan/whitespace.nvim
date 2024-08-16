21dfe3478dced70327d2b08d0b84daec5ebf3610
gavin.xu <gavin.xu@sigmastar.com.cn>
2024-08-16 20:16:41 +0800

    Fix wrong highlight when switching between terminal and code file
    reproduction steps:
    1. type :e to edit a lua file or other file not be ignored
    2. type :terminal to open a terminal buffer
    3. type :e to previous lua file or other file not be ignored
    4. wrong highlight happened
    
    Change-Id: Ib96951e47f16f75cd7a7a7a84f1a9125e6a20c1e

--- lua/whitespace-nvim/init.lua
+++ lua/whitespace-nvim/init.lua
@@ -46,12 +46,21 @@ local function term_open()
   end
 end
 
+local function buf_enter()
+  if vim.tbl_contains(config.ignored_filetypes, vim.bo.filetype) or (config.ignore_terminal and vim.bo.buftype == 'terminal' ) then
+    whitespace.nohighlight()
+  else
+    whitespace.highlight()
+  end
+end
+
 whitespace.setup = function(options)
   config = vim.tbl_extend('force', config, options or {})
 
   vim.api.nvim_create_augroup('whitespace_nvim', {clear = true})
   vim.api.nvim_create_autocmd('FileType', {group = 'whitespace_nvim', pattern = '*', callback = file_type})
   vim.api.nvim_create_autocmd('TermOpen', {group = 'whitespace_nvim', pattern = '*', callback = term_open})
+  vim.api.nvim_create_autocmd('BufEnter', {group = 'whitespace_nvim', pattern = '*', callback = buf_enter})
 end
 
 return whitespace
