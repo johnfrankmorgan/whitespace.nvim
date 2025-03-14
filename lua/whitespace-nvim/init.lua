8bf60b4a6892aa517a78a6014896078ae0c4c642
johnfrankmorgan <john.morgan+copilot@lexisnexisrisk.com>
2025-03-14 08:52:02 +0000

    Autoformat

--- lua/whitespace-nvim/init.lua
+++ lua/whitespace-nvim/init.lua
@@ -1,71 +1,71 @@
 local config = {
-  highlight = "DiffDelete",
-  ignored_filetypes = { "TelescopePrompt", "Trouble", "help", "dashboard" },
-  ignore_terminal = true,
-  return_cursor = true,
+	highlight = "DiffDelete",
+	ignored_filetypes = { "TelescopePrompt", "Trouble", "help", "dashboard" },
+	ignore_terminal = true,
+	return_cursor = true,
 }
 
 local whitespace = {}
 
 local function should_highlight()
-  if vim.bo.buftype == "nofile" then
-    return false
-  end
+	if vim.bo.buftype == "nofile" then
+		return false
+	end
 
-  if config.ignore_terminal and vim.bo.buftype == "terminal" then
-    return false
-  end
+	if config.ignore_terminal and vim.bo.buftype == "terminal" then
+		return false
+	end
 
-  if vim.tbl_contains(config.ignored_filetypes, vim.bo.filetype) then
-    return false
-  end
+	if vim.tbl_contains(config.ignored_filetypes, vim.bo.filetype) then
+		return false
+	end
 
-  return true
+	return true
 end
 
 whitespace.highlight = function()
-  if not vim.fn.hlexists(config.highlight) then
-    error(string.format("highlight %s does not exist", config.highlight))
-  end
+	if not vim.fn.hlexists(config.highlight) then
+		error(string.format("highlight %s does not exist", config.highlight))
+	end
 
-  if should_highlight() then
-    local command = string.format([[match %s /\s\+$/]], config.highlight)
-    vim.cmd(command)
-  else
-    vim.cmd("match")
-  end
+	if should_highlight() then
+		local command = string.format([[match %s /\s\+$/]], config.highlight)
+		vim.cmd(command)
+	else
+		vim.cmd("match")
+	end
 end
 
 whitespace.trim = function()
-  local save_cursor = vim.fn.getpos(".")
+	local save_cursor = vim.fn.getpos(".")
 
-  vim.cmd([[keeppatterns %substitute/\v\s+$//eg]])
+	vim.cmd([[keeppatterns %substitute/\v\s+$//eg]])
 
-  if config.return_cursor then
-    vim.fn.setpos(".", save_cursor)
-  end
+	if config.return_cursor then
+		vim.fn.setpos(".", save_cursor)
+	end
 end
 
 whitespace.setup = function(options)
-  config = vim.tbl_extend("force", config, options or {})
+	config = vim.tbl_extend("force", config, options or {})
 
-  vim.api.nvim_create_augroup("whitespace_nvim", { clear = true })
-  vim.api.nvim_create_autocmd(
-    "FileType",
-    { group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
-  )
-  vim.api.nvim_create_autocmd(
-    "TermOpen",
-    { group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
-  )
-  vim.api.nvim_create_autocmd(
-    "BufEnter",
-    { group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
-  )
-  vim.api.nvim_create_autocmd(
-    "UIEnter",
-    { group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
-  )
+	vim.api.nvim_create_augroup("whitespace_nvim", { clear = true })
+	vim.api.nvim_create_autocmd(
+		"FileType",
+		{ group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
+	)
+	vim.api.nvim_create_autocmd(
+		"TermOpen",
+		{ group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
+	)
+	vim.api.nvim_create_autocmd(
+		"BufEnter",
+		{ group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
+	)
+	vim.api.nvim_create_autocmd(
+		"UIEnter",
+		{ group = "whitespace_nvim", pattern = "*", callback = whitespace.highlight }
+	)
 end
 
 return whitespace
