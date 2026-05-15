d49f9d90cf272e758b64f5be5a8ef8ba76a319d4
Michal Trybus <komar007@gmail.com>
2026-05-15 19:18:06 +0200

    feat: ignore snacks.nvim picker text input by default

--- lua/whitespace-nvim/init.lua
+++ lua/whitespace-nvim/init.lua
@@ -1,6 +1,6 @@
 local config = {
 	highlight = "DiffDelete",
-	ignored_filetypes = { "TelescopePrompt", "Trouble", "help", "dashboard" },
+	ignored_filetypes = { "TelescopePrompt", "snacks_picker_input", "Trouble", "help", "dashboard" },
 	ignore_terminal = true,
 	return_cursor = true,
 }
